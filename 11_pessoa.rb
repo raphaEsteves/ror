8c143378bd7c8aafc4427438a7fcb5f1eb3fb30f
Raphael Martins <raphiusk.rio@gmail.com>
2022-04-21 12:58:11 -0300

    more parameters & tests

--- 11_pessoa.rb
+++ 11_pessoa.rb
@@ -1,9 +1,22 @@
 class Pessoa
     def falar(nome)
-        "Olá, #{nome}!"
+        puts "Olá, #{nome}!"
     end
 #ou com valor padrão
     def carminha( msg = "INFEEEEEEEEEEEERRNO!")
-        msg
+        puts msg
     end
+#Mais de um parâmetro
+    def cumprimentar(periodo, nome)
+        if (periodo == "manhã" || periodo == "Manhã")
+            puts "#{nome}, bom dia!"
+        else
+            puts "#{nome}, boa noite"
+        end
+    end 
 end
+
+p = Pessoa.new
+p.falar("Rapha")
+p.carminha()
+p.cumprimentar("tarde", "Raphael")
