26207465b5263815ede909aa1f81c593b0102b55
Raphael Martins <raphiusk.rio@gmail.com>
2022-04-21 22:14:39 -0300

    Heranca

---
+++ 15_heranca.rb
@@ -0,0 +1,47 @@
+class Pessoa
+    #Cria getters e setters dos atributos
+    attr_accessor :nome, :email
+end
+
+#classe PessoaFisica possui atributos da classe Pessoa
+#Isto é, nome e email.
+class PessoaFisica < Pessoa
+    #Cria getters e setters dos atributos
+    attr_accessor :cpf
+
+    def falar(texto)
+        texto
+    end
+end
+
+#classe PessoaJuridica possui atributos da classe Pessoa
+#Isto é, nome e email.
+class PessoaJuridica < Pessoa
+    #Cria getters e setters dos atributos
+    attr_accessor :cnpj
+
+    def pagar_fornecedor
+        puts "Pagando fornecedor."
+    end
+end
+
+#"Setters"
+p1 = Pessoa.new
+p1.nome = "Rapha"
+p1.email = "rapha@el.com"
+
+pf = PessoaFisica.new
+pf.nome = "Mari"
+pf.email = "mari@ana.com"
+pf.cpf = "56423"
+pf.falar("Oiii")
+
+pj = PessoaJuridica.new
+pj.nome = "Consultoria RM"
+pj.email = "rm@consultoria.com"
+pj.cnpj = "45345"
+
+#"Getters"
+puts "O senhor #{p1.nome}, email é #{p1.email}, sem cpf, e a senhorita #{pf.nome}, email #{pf.email} e cpf #{pf.cpf}, são donos da empresa #{pj.nome}, email #{pj.email} e CNPJ #{pj.cnpj}. Ambos pretendem realizar uma operação."
+pj.pagar_fornecedor
+
